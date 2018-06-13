ce2948e9b3dfbafab864001be5a8197ad16a5c90
Unknown <tanrui106@gmail.com>
2018-06-13 11:48:28 +0800

    整个项目

---
+++ back-end/SceneView/Controllers/messagesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SceneView.Models;
+
+namespace SceneView.Controllers
+{
+    public class messagesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: messages
+        public ActionResult Index()
+        {
+            var message = db.message.Include(m => m.user).Include(m => m.user1);
+            return View(message.ToList());
+        }
+
+        // GET: messages/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            message message = db.message.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.receiverID = new SelectList(db.user, "userID", "password");
+            ViewBag.senderID = new SelectList(db.user, "userID", "password");
+            return View();
+        }
+
+        // POST: messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "messageID,senderID,receiverID,time")] message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.message.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.receiverID = new SelectList(db.user, "userID", "password", message.receiverID);
+            ViewBag.senderID = new SelectList(db.user, "userID", "password", message.senderID);
+            return View(message);
+        }
+
+        // GET: messages/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            message message = db.message.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.receiverID = new SelectList(db.user, "userID", "password", message.receiverID);
+            ViewBag.senderID = new SelectList(db.user, "userID", "password", message.senderID);
+            return View(message);
+        }
+
+        // POST: messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "messageID,senderID,receiverID,time")] message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.receiverID = new SelectList(db.user, "userID", "password", message.receiverID);
+            ViewBag.senderID = new SelectList(db.user, "userID", "password", message.senderID);
+            return View(message);
+        }
+
+        // GET: messages/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            message message = db.message.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            message message = db.message.Find(id);
+            db.message.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ back-end/SceneView/Controllers/scenicPosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SceneView.Models;
+
+namespace SceneView.Controllers
+{
+    public class scenicPosController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: scenicPos
+        public ActionResult Index()
+        {
+            var scenicPos = db.scenicPos.Include(s => s.scenicSpot);
+            return View(scenicPos.ToList());
+        }
+
+        // GET: scenicPos/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            scenicPos scenicPos = db.scenicPos.Find(id);
+            if (scenicPos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scenicPos);
+        }
+
+        // GET: scenicPos/Create
+        public ActionResult Create()
+        {
+            ViewBag.scenicID = new SelectList(db.scenicSpot, "scenicID", "scenicName");
+            return View();
+        }
+
+        // POST: scenicPos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "scenicID,longitue,latitude,address")] scenicPos scenicPos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.scenicPos.Add(scenicPos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.scenicID = new SelectList(db.scenicSpot, "scenicID", "scenicName", scenicPos.scenicID);
+            return View(scenicPos);
+        }
+
+        // GET: scenicPos/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            scenicPos scenicPos = db.scenicPos.Find(id);
+            if (scenicPos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.scenicID = new SelectList(db.scenicSpot, "scenicID", "scenicName", scenicPos.scenicID);
+            return View(scenicPos);
+        }
+
+        // POST: scenicPos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "scenicID,longitue,latitude,address")] scenicPos scenicPos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scenicPos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.scenicID = new SelectList(db.scenicSpot, "scenicID", "scenicName", scenicPos.scenicID);
+            return View(scenicPos);
+        }
+
+        // GET: scenicPos/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            scenicPos scenicPos = db.scenicPos.Find(id);
+            if (scenicPos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scenicPos);
+        }
+
+        // POST: scenicPos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            scenicPos scenicPos = db.scenicPos.Find(id);
+            db.scenicPos.Remove(scenicPos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
