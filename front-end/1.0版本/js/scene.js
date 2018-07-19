2528dbf8ec6c6cb0f4ff3eb67d7892fa327ff907
sukamay <253246887@qq.com>
2018-07-19 16:23:10 +0800

    景点总页面
    加上了页面下标

--- front-end/1.0版本/js/scene.js
+++ front-end/1.0版本/js/scene.js
@@ -12,7 +12,22 @@ function updateDistrict(district){
     }
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState==4 && xmlhttp.status==200){
-            document.getElementById("sceneShow").innerHTML = xmlhttp.responseText;
+            document.getElementById("scene-show-cells").innerHTML = xmlhttp.responseText;
+            //to do 
+            //生成规则范例
+            // <a class="cell" href="index.html">
+			// 				<div class="img-show">
+			// 					<img src="images/32.jpg">
+			// 				</div>
+			// 				<div class="txt">
+			// 					<p class="name">name</p>
+			// 					<div class="score">
+			// 						<i class="socre-show" id="scoreShow"></i>
+			// 						<span class="score-num">score-num</span>
+			// 					</div>
+			// 					<p class="address">address</p>
+			// 				</div>
+			// 			</a>
         }
     }
     xmlhttp.open("POST","/ajax/",true); //to do
