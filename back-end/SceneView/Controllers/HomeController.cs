d5a179acf4cf4de43e5a20df3cc4556fc14eb9b3
yatesliang <yates.lyc@gmail.com>
2018-07-24 20:30:05 +0800

    整合主页、景点主页、登陆页面
    整合第一步

--- back-end/SceneView/Controllers/HomeController.cs
+++ back-end/SceneView/Controllers/HomeController.cs
@@ -11,7 +11,42 @@ namespace SceneView.Controllers
     public class HomeController : Controller
     {
         private Entities db = new Entities();
-        
+        private string province;
+        private string city;
+        private string district;
+
+
+        public class Position
+        {
+            public string province { get; set; }
+            public string city { get; set; }
+            public string district { get; set; }
+        }
+
+        public class CommentView
+        {
+            public string imageSrc { get; set; }
+            public string userName { get; set; }
+            public string commentContent { get; set; }
+            public string city { get; set; }
+
+        }
+
+        public class NoteView
+        {
+            public string noteTitle { get; set; }
+            public string imageSrc { get; set; }
+            public string city { get; set; }
+        }
+        public void getPosition(string province, string city, string district)
+        {
+            var position = new Position();
+            ViewBag.locate = 0;
+            Session["province"] = province;
+            Session["city"] = city;
+            Session["district"] = district;
+            Redirect("~/Home");
+        }
         [HttpGet]
         public ActionResult Index()
         {
@@ -22,95 +57,172 @@ namespace SceneView.Controllers
             else
             {
                 var userID = Session["user"].ToString();
-                user u = db.user.Where(cu => cu.userID == userID).FirstOrDefault<user>();
-                if (u == null)
+                user us = db.user.Where(cu => cu.userID == userID).FirstOrDefault<user>();
+                if (us == null)
                 {
                     return Redirect("/Error");
                 }
                 else
                 {
-                    return View(u);
+                    if (Session["province"] == null || Session["province"].ToString() == ""
+               || Session["city"] == null || Session["city"].ToString() == ""
+               || Session["district"] == null || Session["district"].ToString() == "")
+                    {
+                        Session["needLocation"] = 1;
+                        this.province = "上海市";
+                        this.city = "上海市";
+                        this.district = "嘉定区";
+                    }
+                    else
+                    {
+                        Session["needLocation"] = 0;
+                        this.province = Session["province"].ToString();
+                        this.city = Session["city"].ToString();
+                        this.district = Session["district"].ToString();
+                    }
+
+                    // get spot and position
+                    var resultPos = db.scenicPos.Where(u => u.city == city && u.district == district).FirstOrDefault<scenicPos>();
+                    var resultSpot = db.scenicSpot.Where(u => u.scenicID.Equals(resultPos.scenicID)).ToList<scenicSpot>();
+                    ViewBag.pos = resultPos;
+                    ViewBag.spot = resultSpot;
+
+                    // get recommended images
+                    IList<string> imgSrcs = new List<string>();
+                    foreach (var spot in resultSpot)
+                    {
+                        var images = spot.image.ToList();
+                        foreach (var img in images)
+                        {
+                            string address = img.imageAddress;
+                            imgSrcs.Add(address);
+                        }
+                    }
+                    ViewBag.recommendedSrc = imgSrcs;
+
+                    // get notes
+                    IList<note> notes = new List<note>();
+                    IList<NoteView> noteViews = new List<NoteView>();
+                    foreach (var spot in resultSpot)
+                    {
+                        notes = (from n in db.note where n.scenicID == spot.scenicID select n).ToList();
+                        foreach (var note in notes)
+                        {
+                            var noteView = new NoteView();
+                            noteView.noteTitle = note.title;
+                            noteView.city = resultPos.city;
+                            noteViews.Add(noteView);
+                        }
+                    }
+                    ViewBag.notes = notes;
+                    ViewBag.noteViews = noteViews;
+
+                    // get comments
+                    IList<comment> comments = new List<comment>();
+                    IList<CommentView> commentViews = new List<CommentView>();
+                    foreach (var rspot in resultSpot)
+                    {
+                        var resultComments = db.comment.Where(c => c.scenicID == rspot.scenicID);
+                        comments = resultComments.ToList();
+                        foreach (var c in comments)
+                        {
+                            var cUser = db.user.Where(u => u.userID == c.userID).FirstOrDefault<user>();
+                            var cUserInfo = db.userInfo.Where(ui => ui.userID == cUser.userID).FirstOrDefault<userInfo>();
+                            var spot = db.scenicSpot.Where(s => s.scenicID == c.scenicID).FirstOrDefault<scenicSpot>();
+                            var commentView = new CommentView();
+                            commentView.city = resultPos.city;
+                            commentView.userName = cUserInfo.nickname;
+                            commentView.commentContent = c.commentContent;
+                            commentViews.Add(commentView);
+                        }
+                    }
+                    ViewBag.comments = comments;
+                    ViewBag.commentViews = commentViews;
+
+                    return View(us);
                 }
 
             }
         }
-
-        //public ActionResult Index(Position position)
-        //{
-        //    string city = position.city;
-        //    string district = position.district;
-        //    if(city == null || district == null)
-        //    {
-        //        city = "上海市";
-        //        district = "嘉定区";
-        //        ViewBag.locate = 1;
-        //    }
-        //    else
-        //    {
-        //        ViewBag.locate = 0;
-        //    }
-
-        //    // get spot and position
-        //    var resultPos = db.scenicPos.Where(u => u.city == city && u.district == district).FirstOrDefault<scenicPos>();
-        //    var resultSpot = db.scenicSpot.Where(u => u.scenicID.Equals(resultPos.scenicID)).ToList<scenicSpot>();
-        //    ViewBag.pos = resultPos;
-        //    ViewBag.spot = resultSpot;
-
-        //    // get recommended images
-        //    IList<string> imgSrcs = new List<string>();
-        //    foreach (var spot in resultSpot)
-        //    {
-        //        var images = spot.image.ToList();
-        //        foreach (var img in images)
-        //        {
-        //            string address = img.imageAddress;
-        //            imgSrcs.Add(address);
-        //        }
-        //    }
-        //    ViewBag.recommendedSrc = imgSrcs;
-
-        //    // get notes
-        //    IList<note> notes = new List<note>();
-        //    IList<NoteView> noteViews = new List<NoteView>();
-        //    foreach (var spot in resultSpot)
-        //    {
-        //        notes = (from n in db.note where n.scenicID == spot.scenicID select n).ToList();
-        //        foreach (var note in notes)
-        //        {
-        //            var noteView = new NoteView();
-        //            noteView.noteTitle = note.title;
-        //            noteView.city = resultPos.city;
-        //            noteViews.Add(noteView);
-        //        }
-        //    }
-        //    ViewBag.notes = notes;
-        //    ViewBag.noteViews = noteViews;
-
-        //    // get comments
-        //    IList<comment> comments = new List<comment>();
-        //    IList<CommentView> commentViews = new List<CommentView>();
-        //    foreach (var rspot in resultSpot)
-        //    {
-        //        var resultComments = db.comment.Where(c => c.scenicID == rspot.scenicID);
-        //        comments = resultComments.ToList();
-        //        foreach (var c in comments)
-        //        {
-        //            var cUser = db.user.Where(u => u.userID == c.userID).FirstOrDefault<user>();
-        //            var cUserInfo = db.userInfo.Where(ui => ui.userID == cUser.userID).FirstOrDefault<userInfo>();
-        //            var spot = db.scenicSpot.Where(s => s.scenicID == c.scenicID).FirstOrDefault<scenicSpot>();
-        //            var commentView = new CommentView();
-        //            commentView.city = resultPos.city;
-        //            commentView.userName = cUserInfo.nickname;
-        //            commentView.commentContent = c.commentContent;
-        //            commentViews.Add(commentView);
-        //        }
-        //    }
-        //    ViewBag.comments = comments;
-        //    ViewBag.commentViews = commentViews;
-
-        //    return View();
-        //}
-        
-
     }
 }
+
+//        public ActionResult Index(Position position)
+//        {
+//            string city = position.city;
+//            string district = position.district;
+//            if (city == null || district == null)
+//            {
+//                city = "上海市";
+//                district = "嘉定区";
+//                ViewBag.locate = 1;
+//            }
+//            else
+//            {
+//                ViewBag.locate = 0;
+//            }
+
+//            get spot and position
+//            var resultPos = db.scenicPos.Where(u => u.city == city && u.district == district).FirstOrDefault<scenicPos>();
+//            var resultSpot = db.scenicSpot.Where(u => u.scenicID.Equals(resultPos.scenicID)).ToList<scenicSpot>();
+//            ViewBag.pos = resultPos;
+//            ViewBag.spot = resultSpot;
+
+//            get recommended images
+//            IList<string> imgSrcs = new List<string>();
+//            foreach (var spot in resultSpot)
+//            {
+//                var images = spot.image.ToList();
+//                foreach (var img in images)
+//                {
+//                    string address = img.imageAddress;
+//                    imgSrcs.Add(address);
+//                }
+//            }
+//            ViewBag.recommendedSrc = imgSrcs;
+
+//            get notes
+//            IList<note> notes = new List<note>();
+//            IList<NoteView> noteViews = new List<NoteView>();
+//            foreach (var spot in resultSpot)
+//            {
+//                notes = (from n in db.note where n.scenicID == spot.scenicID select n).ToList();
+//                foreach (var note in notes)
+//                {
+//                    var noteView = new NoteView();
+//                    noteView.noteTitle = note.title;
+//                    noteView.city = resultPos.city;
+//                    noteViews.Add(noteView);
+//                }
+//            }
+//            ViewBag.notes = notes;
+//            ViewBag.noteViews = noteViews;
+
+//            get comments
+//            IList<comment> comments = new List<comment>();
+//            IList<CommentView> commentViews = new List<CommentView>();
+//            foreach (var rspot in resultSpot)
+//            {
+//                var resultComments = db.comment.Where(c => c.scenicID == rspot.scenicID);
+//                comments = resultComments.ToList();
+//                foreach (var c in comments)
+//                {
+//                    var cUser = db.user.Where(u => u.userID == c.userID).FirstOrDefault<user>();
+//                    var cUserInfo = db.userInfo.Where(ui => ui.userID == cUser.userID).FirstOrDefault<userInfo>();
+//                    var spot = db.scenicSpot.Where(s => s.scenicID == c.scenicID).FirstOrDefault<scenicSpot>();
+//                    var commentView = new CommentView();
+//                    commentView.city = resultPos.city;
+//                    commentView.userName = cUserInfo.nickname;
+//                    commentView.commentContent = c.commentContent;
+//                    commentViews.Add(commentView);
+//                }
+//            }
+//            ViewBag.comments = comments;
+//            ViewBag.commentViews = commentViews;
+
+//            return View();
+//        }
+
+
+//    }
+//}
---
+++ back-end/SceneView/Controllers/SceneController.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SceneView.Models;
+namespace SceneView.Controllers
+{
+    public class SceneController : Controller
+    {
+        private Entities db = new Entities();
+        // GET: Scene
+        // public ActionResult Index()
+        // {
+
+        //     return View();
+        // }
+        //[HttpPost]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+            {
+                return View("~/Views/Login/Index.cshtml");
+            }
+            else
+            {
+                var id = Session["user"].ToString();
+                var usern = db.userInfo.Where(u => u.userID == id).FirstOrDefault<userInfo>().nickname;
+                var user = db.user.Where(u => u.userID == id).FirstOrDefault<user>();
+                var spotname = Request.QueryString["sn"];
+                spotname = spotname == "" || spotname == null ? "上海迪士尼度假区" : spotname;
+                Spot scene = new Spot();
+                scene.usern = usern;
+                scene.user = user;
+                var scenicSpot = db.scenicSpot.Where(u => u.scenicName == spotname).FirstOrDefault<scenicSpot>();
+                scene.spot = scenicSpot;
+                if (scenicSpot == null)
+                {
+
+                    ViewBag.flag = -1;
+                    return View();
+                }
+                else
+                {
+                    return View(scene);
+                }
+                
+               
+            }
+        }
+        [HttpGet]
+        public ActionResult UpdateComment()
+        {
+            var spotname = Request.QueryString["sn"];
+            spotname = spotname == "" || spotname == null ? "东方明珠广播电视塔" : spotname;
+            var scenicSpot = db.scenicSpot.Where(u => u.scenicName == spotname).FirstOrDefault<scenicSpot>();
+            var c = new Comment();
+            List<SceneView.Models.comment> commentList = db.comment.Where(u => u.scenicID == scenicSpot.scenicID).OrderByDescending(u => u.commentTime).ToList();
+            
+            List<Comment> comlist = new List<Comment>();
+            foreach (var cn in commentList)
+            {
+                Comment comm = new Comment();
+                comm.userID = cn.userID;
+                comm.commentID = cn.commentID;
+                comm.username = db.userInfo.Where(u => u.userID == cn.userID).FirstOrDefault().nickname;
+                comm.commentContent = cn.commentContent;
+                comm.commentLike = cn.commentLike;
+                comm.commentTime = cn.commentTime;
+                comlist.Add(comm);
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(comlist);
+
+            }
+            return View(comlist);
+
+        }
+        [HttpGet]
+        public ActionResult Blog()
+        {
+            var page = Request["page"];
+            page = page == "" || page == null ? "0" : page;
+            var p = int.Parse(page);
+            var noteList = db.note.ToList();
+            var list = noteList.Skip(p * 6).Take(6).ToList();
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult  UpdateComment(Spot r)
+       {   
+                var comment = new comment();
+                comment.commentContent = r.commentContent;
+                var id = Session["user"].ToString();
+                comment.userID = id;
+                var spotname = Request.QueryString["sn"];
+                spotname = spotname == "" || spotname == null ? "东方明珠广播电视塔" : spotname;
+                var scenicSpot = db.scenicSpot.Where(u => u.scenicName == spotname).FirstOrDefault<scenicSpot>();
+                comment.scenicID = scenicSpot.scenicID;
+                comment.commentLike = 0;
+                System.DateTime currentTime = new System.DateTime();
+                currentTime = System.DateTime.Now;
+                comment.commentTime = currentTime;
+                var com = db.comment;
+                comment.commentID = com.Count() == 0 ? 1 : (com.Select(u => u.commentID).Max() + 1);
+                db.comment.Add(comment);
+                bool saveFailed;
+                do
+                {
+                    saveFailed = false;
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (DbUpdateConcurrencyException ex)
+                    {
+                        saveFailed = true;
+                        ex.Entries.Single().Reload();
+                    }
+                } while (saveFailed);
+            var c = new Comment();
+            
+            List<SceneView.Models.comment> commentList = db.comment.Where(u=>u.scenicID==scenicSpot.scenicID).OrderByDescending(u => u.commentTime).ToList();
+            
+            List<Comment> comlist=new List<Comment>();  
+            foreach (var cn in commentList)
+            {
+                Comment comm = new Comment();
+                comm.userID = cn.userID;
+                comm.commentID = cn.commentID;
+                comm.username = db.userInfo.Where(u => u.userID == cn.userID).FirstOrDefault().nickname;
+                comm.commentContent = cn.commentContent;
+                comm.commentLike = cn.commentLike;
+                comm.commentTime = cn.commentTime;
+                comlist.Add(comm);
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(comlist);
+
+            }
+            return PartialView(comlist);
+
+
+        }
+        public ActionResult SingleNote()
+        {
+            SceneView.Models.note tempnote = new SceneView.Models.note();
+            var noteI= Request.QueryString["nn"];
+            noteI = noteI == ""||noteI==null ? "1" : noteI;
+            var noteID = int.Parse(noteI);
+            
+            var singlenote = db.note.Where(u => u.noteID == noteID).FirstOrDefault();
+            var singl = db.note.Where(u => u.scenicSpot.scenicID == singlenote.scenicID).ToList();
+            foreach(var s in singl)
+            {
+                if (s.noteID == noteID)
+                {
+                    tempnote = s;
+                    break;
+                }
+            }
+            singl.Remove(tempnote);
+            noteList n = new noteList();
+            n.singlenote = singlenote;
+            n.noteL = singl;
+            return View(n);
+        }
+        [HttpPost]
+        public void UpdateLike(string ln,string ni)
+        {
+            decimal likenum = int.Parse(ln);
+            int noteID = int.Parse(ni);
+            var note = db.note.Where(u => u.noteID == noteID).FirstOrDefault();
+            note.noteLike = likenum;
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    ex.Entries.Single().Reload();
+                }
+            }while (saveFailed) ;
+        }
+ 
+        public ActionResult UpdateBlog()
+        {
+            var page = Request["page"];
+            page = page ==""||page==null ? "0" : page;
+            var p = int.Parse(page);
+            var noteList = db.note.ToList();
+            var list = noteList.Skip(p * 6).Take(6).ToList();
+            return PartialView(list);
+        }
+        //[HttpPost]
+        //public ActionResult UpdateBlog()
+        //{
+        //    var page = Request["page"];
+        //    page = page == "" || page == null ? "0" : page;
+        //    var p = int.Parse(page);
+        //    var noteList = db.note.ToList();
+        //    var list = noteList.Skip(p * 6).Take(6).ToList();
+        //    return PartialView(list);
+        //}
+        public class Spot
+        {
+            public string usern { get; set; }
+            public SceneView.Models.user user { get; set; }
+            public SceneView.Models.scenicSpot spot { get; set; }
+            public string commentContent { get; set; }
+        }
+        public class Comment
+        {
+            public string userID { get; set; }
+            public string username { get; set; }
+            public long commentID { get; set; }
+            public string commentContent { get; set;}
+            public decimal commentLike { get; set; }
+            public System.DateTime commentTime { get; set; }
+            
+        }
+        public class noteList
+        {
+            public SceneView.Models.note singlenote { get; set; }
+            public List<SceneView.Models.note> noteL { get; set; }       
+        }
+        
+    }
+}
